bea798ea13122affdcad1f45da3b787862dde189
TheronOliveira <45536620+TheronOliveira@users.noreply.github.com>
2021-10-07 12:53:02 -0300

    Criado o método buscarPetId, atualizarPet e deletarPet da entidade Pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -3,8 +3,12 @@ package petstore;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class Pet {
     String uri = "https://petstore.swagger.io/v2/pet";
@@ -17,15 +21,62 @@ public class Pet {
     public void cadastrarPet() throws java.io.IOException{
         String arquivo = lerJson("src/test/resources/pet1.json");
 
-        RestAssured.given()
-                        .log().all()
-                    .when()
-                        .contentType("application/json")
-                        .body(arquivo)
-                    .then()
-                        .log().all()
-                        .statusCode(200)
-                    ;
+        given()
+                .log().all()
+                .contentType("application/json")
+        .when()
+                .body(arquivo)
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("semanatestapi"))
+                .body("status", containsString("available") )
+        ;
+    }
+    @Test
+    public void buscarPetId(){
+        String petId = "9223372036854775807";
 
+        given()
+                .log().all()
+                .contentType("application/json")
+        .when()
+                .get(uri +"/"+ petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void atualizarPet() throws IOException {
+        String arquivo = lerJson("src/test/resources/pet2.json");
+
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body(arquivo)
+        .when()
+                .put(uri)
+        .then()
+                .statusCode(200)
+                .body("status", is("sold"))
+        ;
+    }
+
+    public void deletarPet(){
+        String petId = "9223372036854775807";
+
+        given()
+                .log().all()
+                .contentType("application/json")
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .statusCode(200)
+                .body("code", is("200"))
+        ;
     }
 }
